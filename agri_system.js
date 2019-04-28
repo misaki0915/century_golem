42e96c0abd67a203641cd793a078bc9576c8f755
Emily <42111747+misaki0915@users.noreply.github.com>
2019-04-29 00:46:30 +0900

    Make layout of question area

--- agri_system.js
+++ agri_system.js
@@ -1,13 +1,15 @@
 let textReadAloud = new Array();
 let questionText = "";
+let questionBlackText = "";
+let questionRedText = "";
 let wordCount = 0;
 let gameNumber = 1;
 
 function set_ready(){
 	//各div代入
-  document.getElementById("game_number").innerHTML= "<font size='6'>" + gameNumber + "</font>問目";
+  document.getElementById("game_number").innerHTML= "<font size='6'>" + gameNumber + "</font>問目：";
 	document.getElementById("start_btn_area").innerHTML = "";
-	document.getElementById("next_area").innerHTML = "<input type='button' value='Next' class='set_next_btn' onclick='setNextText()''>";
+	document.getElementById("next_area").innerHTML = "<input type='button' value='▷' class='set_next_btn' onclick='setNextText()''>";
   document.getElementById("answer_area").innerHTML = "	<form name='answer_form'>答え：<input type='text' name='player_answer' value=''><br><input type='button' value='回答' onclick='answer_judge()'><br></form>";
 
   //問題シャッフル
@@ -26,19 +28,22 @@ function set_ready(){
 
 function setNextText() {
 	if(wordCount > qa[gameNumber - 1][2] - 1){
-		questionText += "<font color='red'>" + textReadAloud[gameNumber - 1][wordCount] + "</font>";
+		questionRedText += textReadAloud[gameNumber - 1][wordCount];
 	} else {
-		questionText += textReadAloud[gameNumber - 1][wordCount];
+		questionBlackText += textReadAloud[gameNumber - 1][wordCount];
 	}
 	wordCount++;
-	document.getElementById("question_text").innerHTML = questionText;
-	document.getElementById("word_count").innerHTML= "<font size='4'>" + wordCount + "</font>文字目";
+	document.getElementById("question_text").innerHTML = "<p>" + questionBlackText + "<font color='red'>" + questionRedText + "</font></p>";
+	document.getElementById("word_count").innerHTML= "<font size='5'>" + wordCount + "</font>文字目";
 }
 
 function answer_judge() {
 	var playerAnswer = document.answer_form.player_answer.value;
   if(playerAnswer === qa[gameNumber - 1][0]) {
-		alert("正解！( ˙ㅂ˙)b");
+		let resultBlack = qa[gameNumber - 1][1].slice(0, qa[gameNumber - 1][2]);
+		let resultRed = qa[gameNumber - 1][1].slice(qa[gameNumber - 1][2], qa[gameNumber - 1][1].length);
+		document.getElementById("qa_result_area").innerHTML = "<div class='qa_result_area_red'>◎正解</div>"
+		document.getElementById("qa_result_area").innerHTML += "<p>" + qa[gameNumber - 1][0] + "：" + resultBlack + resultRed;
 		gameNumber++;
 		wordCount = 0;
 		questionText = textReadAloud[gameNumber - 1][wordCount];
@@ -47,19 +52,22 @@ function answer_judge() {
 		document.getElementById("question_text").innerHTML = questionText;
 		document.getElementById("word_count").innerHTML= "<font size='4'>" + wordCount + "</font>文字目";
 	} else {
-		alert("不正解( ˙ㅂ˙)p")
+		document.getElementById("qa_result_area").innerHTML= "<div class='qa_result_area_blue'>×不正解</div>";
 	}
 }
 
 function retrieval_judge() {
 	var retrievalWord = document.retrieval_form.retrieval.value;
-	document.getElementById("retrieval_text").innerHTML = retrievalWord + "：";
 	for(var ch = 0; ch < qa.length; ch++){
     if(qa[ch][0] == retrievalWord){
 			let resultBlack = qa[ch][1].slice(0, qa[ch][2]);
 			let resultRed = qa[ch][1].slice(qa[ch][2], qa[ch][1].length);
+			document.getElementById("retrieval_result_box").innerHTML = "<div id='retrieval_text'></div>"
+			document.getElementById("retrieval_text").innerHTML = "<span class='box-title'>" + retrievalWord + "</span>";
 		  document.getElementById("retrieval_text").innerHTML += resultBlack + "<font color='red'>" + resultRed + "</font>";
 		  break;
-  	}
+  	} else{
+			document.getElementById("retrieval_result_box").innerHTML = "<font color='red'>「" + retrievalWord + "」は職業一覧から見つかりませんでした。</font>"
+		}
   }
 }
