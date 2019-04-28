722b80e7900cedcf3385aed1564877f6ccf99ff3
Emily <42111747+misaki0915@users.noreply.github.com>
2019-04-28 18:30:59 +0900

    Add retrieval

--- agri_system.js
+++ agri_system.js
@@ -39,10 +39,22 @@ function answer_judge() {
 		questionText = textReadAloud[gameNumber - 1][wordCount];
 		wordCount++;
 		document.getElementById("game_number").innerHTML= "<font size='6'>" + gameNumber + "</font>問目";
-		document.getElementById("question_text").innerHTML = questionText
+		document.getElementById("question_text").innerHTML = questionText;
 		document.getElementById("word_count").innerHTML= "<font size='4'>" + wordCount + "</font>文字目";
 	} else {
 		alert("不正解( ˙ㅂ˙)p")
 	}
+}
 
+function retrieval_judge() {
+	var retrievalWord = document.retrieval_form.retrieval.value;
+	document.getElementById("retrieval_text").innerHTML = retrievalWord + "：";
+	for(var ch = 0; ch < qa.length; ch++){
+    if(qa[ch][0] == retrievalWord){
+			let resultBlack = qa[ch][1].slice(0, qa[ch][2]);
+			let resultRed = qa[ch][1].slice(qa[ch][2], qa[ch][1].length);
+		  document.getElementById("retrieval_text").innerHTML += resultBlack + "<font color='red'>" + resultRed + "</font>";
+		  break;
+  	}
+  }
 }
