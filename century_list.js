f96776d2b0510bc9387304ff74f740356b19a7bc
Emily <42111747+misaki0915@users.noreply.github.com>
2019-05-02 01:45:56 +0900

    first commit

---
+++ century_list.js
@@ -0,0 +1,98 @@
+//問題と解答
+golem = new Array();
+golem[0] = [10,0,5,0,0,0];
+golem[1] = [14,3,1,1,1,0];
+golem[2] = [17,2,0,2,2,0];
+golem[3] = [14,0,3,2,0,0];
+golem[4] = [8,2,3,0,0,0];
+golem[5] = [12,0,0,4,0,0];
+golem[6] = [13,0,2,3,0,0];
+golem[7] = [15,0,0,5,0,0];
+golem[8] = [17,0,0,3,2,0];
+
+for(var j = golem.length - 1; j >= 0; j--){
+  jewelText = "<font color='yellow'>";
+  for(var i = 0; i < golem[j][1]; i++){
+    jewelText += "●"
+  };
+  jewelText += "</font><font color='green'>"
+  for(var i = 0; i < golem[j][2]; i++){
+    jewelText += "●"
+  };
+  jewelText += "</font><font color='blue'>"
+  for(var i = 0; i < golem[j][3]; i++){
+    jewelText += "●"
+  };
+  jewelText += "</font><font color='red'>"
+  for(var i = 0; i < golem[j][4]; i++){
+    jewelText += "●"
+  };
+  golem[j][6] = jewelText + "</font>";
+}
+
+for (var i = golem.length - 1; i >= 0; i--){
+  var j = Math.floor(Math.random() * (i + 1));
+  [golem[i], golem[j]] = [golem[j], golem[i]];
+}
+
+handCard = new Array();
+handCard[0] = [0,0,0,0,0,0,0,0,2,0,"",0,0,0,0];
+handCard[1] = [0,0,0,0,2,0,0,0,0,0,"",0,0,0,0];
+handCard[2] = [0,0,0,0,0,0,0,0,3,0,"",0,0,0,0];
+handCard[3] = [0,0,0,0,4,0,0,0,0,0,"",0,0,0,0];
+handCard[4] = [0,2,0,0,2,0,0,1,0,0,"",0,0,0,0];
+handCard[5] = [0,0,0,1,3,0,1,0,0,0,"",0,0,0,0];
+handCard[6] = [0,0,0,0,0,2,0,0,0,0,"",0,0,0,0];
+handCard[7] = [0,0,2,0,0,0,0,2,0,0,"",0,0,0,0];
+handCard[8] = [0,0,3,0,0,0,0,3,0,0,"",0,0,0,0];
+handCard[9] = [0,3,0,0,0,0,0,2,0,0,"",0,0,0,0];
+handCard[10] = [0,0,0,1,1,1,1,0,0,0,"",0,0,0,0];
+handCard[11] = [4,0,0,0,0,0,1,1,0,0,"",0,0,0,0];
+handCard[12] = [0,0,0,1,0,3,0,0,0,0,"",0,0,0,0];
+
+for (var i = handCard.length - 1; i > 2; i--){
+  var j = Math.floor(Math.random() * (i + 1) + 2);
+  [handCard[i], handCard[j]] = [handCard[j], handCard[i]];
+}
+
+for(let possession = 0; 0 < handCard.length; possession++){
+  if(handCard[possession][8] > 0){
+    let text = "";
+    for(var i = 0; i < handCard[possession][8]; i++){
+      text += "⬆︎"
+    };
+    handCard[possession][10] = text;
+  } else {
+    let jewelBefore = "";
+    for(var i = 0; i < handCard[possession][0]; i++){
+      jewelBefore += "<font color='yellow'>●</font>"
+    };
+    for(var i = 0; i < handCard[possession][1]; i++){
+      jewelBefore += "<font color='green'>●</font>"
+    };
+    for(var i = 0; i < handCard[possession][2]; i++){
+      jewelBefore += "<font color='blue'>●</font>"
+    };
+    for(var i = 0; i < handCard[possession][3]; i++){
+      jewelBefore += "<font color='red'>●</font>"
+    };
+    if(jewelBefore !== ""){
+      jewelBefore += "➡︎";
+    }
+
+    let jewelAfter = "";
+    for(var i = 0; i < handCard[possession][4]; i++){
+      jewelAfter += "<font color='yellow'>●</font>"
+    };
+    for(var i = 0; i < handCard[possession][5]; i++){
+      jewelAfter += "<font color='green'>●</font>"
+    };
+    for(var i = 0; i < handCard[possession][6]; i++){
+      jewelAfter += "<font color='blue'>●</font>"
+    };
+    for(var i = 0; i < handCard[possession][7]; i++){
+      jewelAfter += "<font color='red'>●</font>"
+    };
+    handCard[possession][10] = jewelBefore + jewelAfter;
+  }
+}
---
+++ century_system.js
@@ -0,0 +1,239 @@
+let jewelColorCount = new Array;
+jewelColorCount[0] = 0;
+jewelColorCount[1] = 0;
+jewelColorCount[2] = 0;
+jewelColorCount[3] = 0;
+
+let golemPossession = new Array;
+
+let jewelText;
+let gameTurn = 1;
+let golemNumber = 0;
+let marketCount = 4;
+let golemMarketPlase = 0;
+
+function set_ready(){
+  for(golemNumber = 0; golemNumber < 5; golemNumber++){
+    golemPossession[golemNumber] = "<p>【" + golem[golemNumber][0] + "点】" + golem[golemNumber][6] + "<a onclick='purchase_golem(" + golemNumber + ")' class=''> [購入]</a>";
+  document.getElementById("golem_market").innerHTML += golemPossession[golemNumber];
+  }
+  document.getElementById("hand_card_area").innerHTML = "<li>" + handCard[0][10] + "<a onclick='use_card(" + 0 + ")' class=''> [使う]</a></li>";
+  document.getElementById("hand_card_area").innerHTML += "<li>" + handCard[1][10] + "<a onclick='use_card(" + 1 + ")' class=''> [使う]</a></li>";
+
+  for(let cardPossession = 2; cardPossession < 8; cardPossession++){
+    let position = cardPossession - 2;
+    document.getElementById("card_market").innerHTML += "<li>" + handCard[cardPossession][10] + "<a onclick='payment_jewel(" + position + "," + 0 + "," + position + "," + cardPossession + ")' class=''> [購入]</a></li>";
+  };
+  document.getElementById("game_turn").innerHTML = gameTurn + "turn";
+}
+
+function use_card(cardPossession){// handCard[cardPossession] を使用する
+  if(handCard[cardPossession][8] > 0){
+    gradeup_jewel(1, handCard[cardPossession][8]);
+  } else if(jewelColorCount[0] >= handCard[cardPossession][0] && jewelColorCount[1] >= handCard[cardPossession][1] && jewelColorCount[2] >= handCard[cardPossession][2] && jewelColorCount[3] >= handCard[cardPossession][3]){
+    for(var x = 0; x < 4; x++){
+      jewelColorCount[x] = jewelColorCount[x] - handCard[cardPossession][x];
+      jewelColorCount[x] = jewelColorCount[x] + handCard[cardPossession][x + 4];
+    }
+    jewel_description();
+    gameTurn++;
+    document.getElementById("game_turn").innerHTML = gameTurn + "turn";
+  } else {
+    alert("使えないよ┏( .-. ┏) ┓")
+  }
+}
+
+
+function gradeup_jewel(count, allCount, gradeupColor){
+  switch (gradeupColor) {
+    case 0:
+      jewelColorCount[0]--;
+      jewelColorCount[1]++;
+      break;
+
+    case 1:
+      jewelColorCount[1]--;
+      jewelColorCount[2]++;
+      break;
+
+    case 2:
+      jewelColorCount[2]--;
+      jewelColorCount[3]++;
+      break;
+
+    default:
+    break;
+  }
+  if(count <= allCount){
+    document.getElementById("gradeup_jewel_area").innerHTML = count + "/" + allCount + "回目のグレードアップ";
+    count++;
+    let gradeupSelect = "";
+    for(var i = 0; i < jewelColorCount[0]; i++){
+      gradeupSelect += "<font color='yellow'><a onclick='gradeup_jewel(" + count + "," + allCount + ", 0)' class=''>●</a></font>";
+    };
+    for(var i = 0; i < jewelColorCount[1]; i++){
+      gradeupSelect += "<font color='green'><a onclick='gradeup_jewel(" + count + "," + allCount + ", 1)' class=''>●</a></font>";
+    };
+    for(var i = 0; i < jewelColorCount[2]; i++){
+      gradeupSelect += "<font color='blue'><a onclick='gradeup_jewel(" + count + "," + allCount + ", 2)' class=''>●</a></font>";
+    };
+    for(var i = 0; i < jewelColorCount[3]; i++){
+      gradeupSelect += "<font color='red'>●</a></font>";
+    };
+    document.getElementById("gradeup_jewel_area").innerHTML += gradeupSelect + "<a onclick='gradeup_jewel(" + count + "," + allCount + ")' class=''>skip</a></font>";;
+  } else {
+    document.getElementById("gradeup_jewel_area").innerHTML = "";
+    gameTurn++;
+    document.getElementById("game_turn").innerHTML = gameTurn + "turn";
+  }
+  jewel_description();
+}
+
+function purchase_golem(golemNumber){
+  if(jewelColorCount[0] >= golem[golemNumber][1] && jewelColorCount[1] >= golem[golemNumber][2] && jewelColorCount[2] >= golem[golemNumber][3] && jewelColorCount[3] >= golem[golemNumber][4]){
+    alert(golem[golemNumber][0] + "点ゴーレムを購入しました");
+    gameTurn++;
+    document.getElementById("game_turn").innerHTML = gameTurn + "turn";
+
+    for(var x = 0; x < 4; x++){
+      jewelColorCount[x] = jewelColorCount[x] - golem[golemNumber][x + 1];
+    }
+
+    jewel_description();
+    golem[golemNumber][5] = 1;
+    document.getElementById("golem_market").innerHTML = "";
+    let j = 0;
+    for(i = 0; i < golem.length, j < 5; i++){
+      if(golem[i][5] == 0){
+        if(j == 0){
+          document.getElementById("golem_market").innerHTML += "<p>⑶</p>"
+        } else if(j == 1){
+          document.getElementById("golem_market").innerHTML += "<p>⑴</p>"
+        }
+        document.getElementById("golem_market").innerHTML += "<p>【" + golem[i][0] + "点】" + golem[i][6] + "<a onclick='purchase_golem(" + i + ")' class=''> [購入]</a></p>";
+        j++;
+      }
+    }
+  } else {
+    alert("足りないよ( ˙ㅂ˙)ﾉ");
+  }
+}
+
+function payment_jewel(position, count, allCount, cardPossession, colorNumber){
+  let total = jewelColorCount[0] + jewelColorCount[1] + jewelColorCount[2] + jewelColorCount[3];
+  if(count > 0){
+    jewelColorCount[colorNumber] = jewelColorCount[colorNumber] - 1;
+    jewel_description();
+    document.getElementById("payment").innerHTML = "カード購入のため、" + position + "個のジュエルが必要です（" + count + "個目）";
+    //jewelの設置
+    let j = 0;
+    for(i = 2; i < handCard.length - 1, j < 5; i++){
+      if(handCard[i][9] == 0 && i !== cardPossession){
+        j++;
+        if(j == count){
+          let putJewel = colorNumber + 11;
+          handCard[i][putJewel]++;
+          break;
+        }
+      }
+    }
+  }
+
+  if(count == 0 && position > total){
+    document.getElementById("payment").innerHTML = "";
+    alert("ジュエルが足りない( ˙ㅂ˙)");
+  } else if(position == count){
+    purchase_card(cardPossession);
+  } else {
+    document.getElementById("payment").innerHTML = "カード購入のため、" + position + "個のジュエルが必要です（" + count + "個目）";
+    count++;
+    let jewelSelect = "";
+    for(var i = 0; i < jewelColorCount[0]; i++){
+      jewelSelect += "<font color='yellow'><a onclick='payment_jewel(" + position + "," + count + "," + allCount + "," + cardPossession + "," + 0 + ")' class=''>●</a></font>";
+    };
+    for(var i = 0; i < jewelColorCount[1]; i++){
+      jewelSelect += "<font color='green'><a onclick='payment_jewel(" + position + "," + count + "," + allCount + "," + cardPossession + "," + 1 + ")' class=''>●</a></font>";
+    };
+    for(var i = 0; i < jewelColorCount[2]; i++){
+      jewelSelect += "<font color='blue'><a onclick='payment_jewel(" + position + "," + count + "," + allCount + "," + cardPossession + "," + 2 + ")' class=''>●</a></font>";
+    };
+    for(var i = 0; i < jewelColorCount[3]; i++){
+      jewelSelect += "<font color='red'><a onclick='payment_jewel(" + position + "," + count + "," + allCount + "," + cardPossession + "," + 3 + ")' class=''>●</a></font>";
+    };
+    document.getElementById("payment").innerHTML += jewelSelect + "<a onclick='gradeup_jewel()' class=''>cancel</a></font>";//cancel 未実装
+  }
+}
+
+function purchase_card(cardNumber){
+  document.getElementById("hand_card_area").innerHTML += "<li>" + handCard[cardNumber][10] + "<a onclick='use_card(" + cardNumber + ")' class=''> [使う]</a></li>";
+  handCard[cardNumber][9] = 1;//player1購入
+  gameTurn++;
+  document.getElementById("game_turn").innerHTML = gameTurn + "turn";
+  let j = 0;
+  document.getElementById("card_market").innerHTML = "";
+  document.getElementById("payment").innerHTML = "購入しました！";
+  for(i = 2; i < handCard.length - 1, j < 6; i++){
+    if(handCard[i][9] == 0){
+
+      //設置jewel描写
+      handCard[i][15] = "";
+      for(x = 11; x < 15; x++){
+        jewelText = "";
+        for(var k = 0; k < handCard[i][x]; k++){
+          jewelText += "●"
+        };
+        switch (x) {
+          case 11:
+            handCard[i][15] += "<font color='yellow'>" + jewelText + "</font>";
+            break;
+
+          case 12:
+            handCard[i][15] += "<font color='green'>" + jewelText + "</font>";
+            break;
+
+          case 13:
+            handCard[i][15] += "<font color='blue'>" + jewelText + "</font>";
+            break;
+
+          case 14:
+            handCard[i][15] += "<font color='red'>" + jewelText + "</font>";
+            break;
+
+          default: alert("error");
+        }
+      }
+
+      document.getElementById("card_market").innerHTML += "<li>" + handCard[i][10] + "<a onclick='payment_jewel(" + j + "," + 0 + "," + j + "," + i + ")' class=''> [購入]</a> " + handCard[i][15] + "</li>";
+      j++;
+    }
+  }
+}
+
+function jewel_description(){
+  document.getElementById("jewel_area").innerHTML = "";
+  for(x = 0; x < 4; x++){
+    jewelText = "";
+    for(var i = 0; i < jewelColorCount[x]; i++){
+      jewelText += "●"
+    };
+    switch (x) {
+      case 0:
+        document.getElementById("jewel_area").innerHTML += "<font color='yellow'>" + jewelText + "</font>";
+        break;
+
+      case 1:
+        document.getElementById("jewel_area").innerHTML += "<font color='green'>" + jewelText + "</font>";
+        break;
+
+      case 2:
+        document.getElementById("jewel_area").innerHTML += "<font color='blue'>" + jewelText + "</font>";
+        break;
+
+      case 3:
+        document.getElementById("jewel_area").innerHTML += "<font color='red'>" + jewelText + "</font>";
+        break;
+
+      default: alert("error");
+    }
+  }
+}
